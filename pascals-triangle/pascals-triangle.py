0b6a19f6bf59726a1c78c806ce87f366b6462a2b
TinKerBellBass <93515608+jongwon-hyun@users.noreply.github.com>
2022-05-11 08:44:01 +0900

    Time: 29 ms, Memory: 14 MB - LeetHub

---
+++ pascals-triangle/pascals-triangle.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = []
+
+        if numRows == 1:
+            return([[1]])
+        elif numRows == 2:
+            return([[1],[1,1]])
+        else:
+            result = [[1],[1,1]]
+            for i in range(2, numRows):
+                tmp = [1]
+                for j in range(i - 1):
+                    tmp.append(result[i - 1][j] + result[i - 1][j + 1])
+                tmp.append(1)
+                result.append(tmp)
+
+        return result
