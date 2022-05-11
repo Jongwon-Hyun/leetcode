73cd1a8e80f0a2226b73c7fbe9d9f21f808432d9
TinKerBellBass <93515608+jongwon-hyun@users.noreply.github.com>
2022-05-11 09:43:54 +0900

    Time: 35 ms, Memory: 13.8 MB - LeetHub

--- add-binary/add-binary.py
+++ add-binary/add-binary.py
@@ -1,33 +1,33 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         # simple, pythonic
-#         aBinaryNum = int(a, 2)
-#         bBinaryNum = int(b, 2)
+        aBinaryNum = int(a, 2)
+        bBinaryNum = int(b, 2)
         
-#         sum = aBinaryNum + bBinaryNum
+        sum = aBinaryNum + bBinaryNum
         
-#         return bin(sum)[2:]
+        return bin(sum)[2:]
 
         # no pythonic
-        def convertToDecimal(n: str) -> int:
-            result = 0
-            powNum = 0
-            for i in range(len(n)- 1, -1, -1):
-                result += int(n[i]) * (2 ** powNum)
-                powNum += 1
-            return result
+#         def convertToDecimal(n: str) -> int:
+#             result = 0
+#             powNum = 0
+#             for i in range(len(n)- 1, -1, -1):
+#                 result += int(n[i]) * (2 ** powNum)
+#                 powNum += 1
+#             return result
                 
-        def convertToBinary(n: int) -> str:
-            result = ""
-            while n >= 2:
-                mod = n % 2
-                n = n // 2
-                result += str(mod)
-            result += str(n)
-            return result[::-1]
+#         def convertToBinary(n: int) -> str:
+#             result = ""
+#             while n >= 2:
+#                 mod = n % 2
+#                 n = n // 2
+#                 result += str(mod)
+#             result += str(n)
+#             return result[::-1]
         
         
-        sum = convertToDecimal(a) + convertToDecimal(b)
+#         sum = convertToDecimal(a) + convertToDecimal(b)
       
-        return convertToBinary(sum)
+#         return convertToBinary(sum)
                 
