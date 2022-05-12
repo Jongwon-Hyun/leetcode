28efb748c92d0bf05e08691fe3ceb1753b04e63c
TinKerBellBass <93515608+jongwon-hyun@users.noreply.github.com>
2022-05-13 08:50:15 +0900

    Time: 210 ms, Memory: 18.3 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -3,14 +3,15 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-#         pythonic
-        # s.reverse()
-    
-        i = 0
-        j = len(s) - 1
-        while i < j:
-            s[i], s[j] = s[j], s[i]
-            i += 1
-            j -= 1
+        #  pythonic
+        s.reverse()
+        
+        # no pythoic
+        # i = 0
+        # j = len(s) - 1
+        # while i < j:
+        #     s[i], s[j] = s[j], s[i]
+        #     i += 1
+        #     j -= 1
         
             
